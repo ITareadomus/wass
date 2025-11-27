4785e3135615253b65661b6d8cef4bc604e8fddb
IT Department AREA DOMUS <it@areadomus.it>
2025-11-27 15:15:09 +0000

    Restored to '0685e37d6a3baf79bf51e31d138bf70b8989a773'
    Replit-Restored-To: 0685e37d6a3baf79bf51e31d138bf70b8989a773

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,29 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { queryClient } from "./lib/queryClient";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// Suppress react-beautiful-dnd defaultProps warning (non-blocking, deprecation notice only)
-const originalWarn = console.warn;
-console.warn = (...args) => {
-  if (
-    typeof args[0] === 'string' &&
-    args[0].includes('Support for defaultProps will be removed from memo components')
-  ) {
-    return;
-  }
-  originalWarn.apply(console, args);
-};
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <App />
-      </TooltipProvider>
-    </QueryClientProvider>
-  </React.StrictMode>
-);
+createRoot(document.getElementById("root")!).render(<App />);
